9ef314486e172141d1a18959deb554964edf38da
Bruna Soncini <soncinibruna@gmail.com>
2026-05-23 17:29:18 -0300

    feat: conta as frequências de cada caractere da lista ordenada

--- huffman.pl
+++ huffman.pl
@@ -6,12 +6,19 @@
 % ====================================
 
 
-% 1.Lê o arquivo e retorna uma lista de caracteres
+% 1. Lê o arquivo e retorna uma lista de caracteres
 ler_arquivo(NomeArquivo, Caracteres) :-
     open(NomeArquivo, read, Stream),
     read_string(Stream, _, Texto),
     close(Stream),
     string_chars(Texto, Caracteres).
+
+% a. Ignora espaços e quebras de linha ('\n') do arquivo 
+filtrar([], []).
+filtrar([], []).
+filtrar([' '|Cs], R) :- filtrar(Cs, R).
+filtrar(['\n'|Cs], R) :- filtrar(Cs, R).
+filtrar([C|Cs], [C|R]) :- filtrar(Cs, R).
 
 % 2. Cria uma tabela estatística com a quantidade de cada letra no texto
 % a. Ordena o texto por merge sort:
@@ -22,13 +29,18 @@ merge_sort(L, R) :- split(L, L1, L2), merge_sort(L1, R1), merge_sort(L2, R2), in
 intercala([], L, L).
 intercala(L, [], L).
 intercala([A|As], [B|Bs], [A|X]) :- A @=< B, intercala(As, [B|Bs], X).
-intercala([A|As], [B|Bs], [B|X]) :- intercala([A|As], Bs, X).
+intercala([A|As], [B|Bs], [B|X]) :- A @> B, intercala([A|As], Bs, X).
 
 split([], [], []).
 split([A], [A], []).
 split([A,B|X], [A|As], [B|Bs]) :- split(X, As, Bs).
 
-% b. Contar cada caractere da lista ordenada
+% b. Contar cada caractere da lista ordenada (RLE)
+conta_frequencia([], []).
+conta_frequencia([E|Es], R) :- conta_frequencia(Es, E, 1, R).
+conta_frequencia([], E, N, [(E, N)]).
+conta_frequencia([E|Es], E, N, R) :- N2 is N + 1, conta_frequencia(Es, E, N2, R).
+conta_frequencia([A|As], E, N, [(E, N)|R]) :- conta_frequencia(As, A, 1, R).
 
 % 3. Cria a árvore de Huffman
 
@@ -39,10 +51,16 @@ split([A,B|X], [A|As], [B|Bs]) :- split(X, As, Bs).
 % 6. Gera o arquivo 'out'
 
 % Função principal
-:- initialization(main).
+:- initialization(iniciar).
 
-main :-
+iniciar :-
     ler_arquivo('in', Entrada),
-    merge_sort(Entrada, TextoOrdenado),
+    filtrar(Entrada, EntradaFiltrada),
+    merge_sort(EntradaFiltrada, TextoOrdenado),
+
     write('Texto Ordenado: '),
-    writeln(TextoOrdenado).
+    writeln(TextoOrdenado),
+
+    conta_frequencia(TextoOrdenado, Frequencia),
+    write('Contagem de caracteres: '),
+    writeln(Frequencia).
